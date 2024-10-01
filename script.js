2e3f533b1f7eb966594332ccfbfd78e0b01f299f
Oleksii Suprun <59663601+alexeysuprun@users.noreply.github.com>
2024-10-01 12:58:37 +0300

    added clearCookie
    added clearCookie

--- script.js
+++ script.js
@@ -8,10 +8,14 @@ app.get('/', (req, res) => {
 })
 
 app.get('/path1', (req, res) => {
+  // res.cookie('name', 'tobi', { domain: '.example.com', path: '/admin', secure: true })
+  res.cookie('rememberme', '1', { expires: new Date(Date.now() + 900000), httpOnly: true })
   res.send(`Path1: I have been sent these cookies: ${req.headers.cookie}`)
 })
 
 app.get('/path2', (req, res) => {
+  res.clearCookie('rememberme', '1', { expires: new Date(Date.now() + 900000), httpOnly: true })
+  res.clearCookie('setfromserver', '1')
   res.send(`Path2: I have been sent these cookies: ${req.headers.cookie}`)
 })
 
